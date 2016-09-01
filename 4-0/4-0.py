c1df2093a561daa74291c7cad6f225042a83936f
Denis <denishqua@gmail.com>
2016-09-01 15:52:27 -0700

    first commit

---
+++ 4-0/4-0.py
@@ -0,0 +1,48 @@
+def initial_position():
+	return 2000
+
+def primitive(pos):
+	if pos == 0:
+		return "LOSE"
+	return "UNDETERMINED"
+
+def gen_moves(pos):
+	if pos > 1:
+		return [-1, -2]
+	return [-1]
+
+def do_move(pos, move):
+	return pos + move
+
+def solve():
+	def get_next_moves(pos):
+		moves = gen_moves(pos)
+		return [do_move(pos, move) for move in moves]
+
+	solution = {}
+	curr = initial_position()
+	queue = [curr]
+	queue = get_next_moves(curr) + queue
+
+	def get_solution_from_children(position):
+		children = get_next_moves(position)
+		if all(child in solution.keys() for child in children):
+			if any(solution[child] == "LOSE" for child in children):
+				return "WIN"
+			return "LOSE"
+		return "UNDETERMINED"
+
+	while queue:
+		curr = queue.pop(0)
+		if not (curr in solution.keys()):
+			prim = primitive(curr)
+			children_sol = get_solution_from_children(curr)
+			if prim != "UNDETERMINED":
+				solution[curr] = prim
+			elif children_sol != "UNDETERMINED":
+				solution[curr] = children_sol
+			else:
+				queue = get_next_moves(curr) + [curr] + queue
+	return solution
+
+print solve()
